c71f585e69c09e23b4bdbe60a3eec2ac1f4d3d6f
Dane Tuso <dane@witcontests.com>
2024-08-15 20:34:43 -0700

    test search baseurl fix

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -36,8 +36,51 @@ var store = [
         {%- endif -%}
         "categories": {{ doc.categories | jsonify }},
         "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }} // Using absolute_url here ensures that the correct URL is always used.
+        "url": {{ doc.url | absolute_url | jsonify }}
       } {%- unless forloop.last and l -%}, {%- endunless -%}
     {%- endfor -%}
   {%- endfor -%}
 ];
+
+$(document).ready(function () {
+  var siteBaseurl = '/pasta-piracy';
+
+  $('input#search').on('keyup', function () {
+    var resultdiv = $('#results');
+    var query = $(this).val().toLowerCase();
+    var result =
+      idx.query(function (q) {
+        query.split(lunr.tokenizer.separator).forEach(function (term) {
+          q.term(term, { boost: 100 })
+          if (query.lastIndexOf(" ") != query.length - 1) {
+            q.term(term, { usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
+          }
+          if (term != "") {
+            q.term(term, { usePipeline: false, editDistance: 1, boost: 1 })
+          }
+        });
+      });
+    resultdiv.empty();
+    resultdiv.prepend('<p class="results__found">' + result.length + ' result(s) found</p>');
+    for (var item in result) {
+      var ref = result[item].ref;
+      var itemUrl = store[ref].url;
+
+      // Ensure correct base URL is used for search result links on GitHub Pages
+      if (window.location.hostname === "danetuso.github.io") {
+        itemUrl = siteBaseurl + itemUrl;
+      }
+
+      var searchitem =
+        '<article class="entry">' +
+        '<h3 class="entry-title">' +
+        '<a href="' + itemUrl + '">' + store[ref].title + '</a>' +
+        '</h3>' +
+        '<div class="entry-excerpt">' +
+        '<p>' + store[ref].excerpt.split(" ").splice(0, 20).join(" ") + '...</p>' +
+        '</div>' +
+        '</article>';
+      resultdiv.append(searchitem);
+    }
+  });
+});
